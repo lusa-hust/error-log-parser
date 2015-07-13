cebfc1df0735bf903dc34f0ab4d01577e7296871
Sam Hoang Van <samhv.ict@gmail.com>
2015-07-13 10:33:41 +0700

    MOD: edit name of function and variable

--- parsers.py
+++ parsers.py
@@ -1,72 +1,71 @@
 import parse
-import sys
 
 
 FORMAT_STRING = {
     'HAPROXY':
-        "{ip}:{port} \
-        [{times}] \
-        {frontend_name_transport} \
-        {backend_name}/{server_name} \
-        {tq}/{tw}/{tc}/{tr}/{tt} \
-        {status_code} \
-        {bytes_sent} \
-        {captured_request_cookie} \
-        {captured_response_cookie} \
-        {country} \
-        {actconn}/{feconn}/{beconn}/{srv_conn}/{retries} \
-        {srv_queue}/{backend_queue} \
-        {{{other}}} \"{http_request}\"",
+"{ip}:{port} \
+[{times}] \
+{frontend_name_transport} \
+{backend_name}/{server_name} \
+{tq}/{tw}/{tc}/{tr}/{tt} \
+{status_code} \
+{bytes_sent} \
+{captured_request_cookie} \
+{captured_response_cookie} \
+{country} \
+{actconn}/{feconn}/{beconn}/{srv_conn}/{retries} \
+{srv_queue}/{backend_queue} \
+{{{other}}} \"{http_request}\"",
 
     'LIGHTTPD':
-        "{ip} \
-        [{time}] \
-        \"{http_request}\" \
-        {status_code} \
-        {byte_sent} \
-        \"{referer}\" \
-        \"{user_agent}\" \
-        {byte_in} \
-        {byte_out} \
-        {time_used} \
-        {request_hostname}",
+"{ip} \
+[{time}] \
+\"{http_request}\" \
+{status_code} \
+{byte_sent} \
+\"{referer}\" \
+\"{user_agent}\" \
+{byte_in} \
+{byte_out} \
+{time_used} \
+{request_hostname}",
 
     'NGINX':
-        "{ip} \
-        {http_x_forwarded_for} \
-        [{time}] \
-        {http_host} \
-        \"{http_request}\" \
-        {status_code} \
-        {bytes_sent} \
-        \"{http_referer}\" \
-        {http_user_agentt}",
+"{ip} \
+{http_x_forwarded_for} \
+[{time}] \
+{http_host} \
+\"{http_request}\" \
+{status_code} \
+{bytes_sent} \
+\"{http_referer}\" \
+{http_user_agentt}",
 
     'ATS':
-        "{time} RESPONSE: sent \
-        {ip} status \
-        {status_code} \
-        ({accelerator}) for \
-        \'{http_referer}\'",
+"{time} RESPONSE: sent \
+{ip} status \
+{status_code} \
+({accelerator}) for \
+\'{http_referer}\'",
 
     'HAPROXY2':
-        "{ip}:{port} \
-        [{times}] \
-        {frontend_name_transport} \
-        {backend_name}/{server_name} \
-        {tq}/{tw}/{tc}/{tr}/{tt} \
-        {status_code} \
-        {bytes_sent} \
-        {captured_request_cookie} \
-        {captured_response_cookie} \
-        {country} \
-        {actconn}/{feconn}/{beconn}/{srv_conn}/{retries} \
-        {srv_queue}/{backend_queue} \
-        \"{http_request}\""
+"{ip}:{port} \
+[{times}] \
+{frontend_name_transport} \
+{backend_name}/{server_name} \
+{tq}/{tw}/{tc}/{tr}/{tt} \
+{status_code} \
+{bytes_sent} \
+{captured_request_cookie} \
+{captured_response_cookie} \
+{country} \
+{actconn}/{feconn}/{beconn}/{srv_conn}/{retries} \
+{srv_queue}/{backend_queue} \
+\"{http_request}\""
 }
 
 
-def log_parsers(mode, string):
+def log_parsers(mode, input_string):
 
     if mode == 1:
         format_string_s = FORMAT_STRING['HAPROXY']
@@ -81,23 +80,22 @@ def log_parsers(mode, string):
     else:
         raise 'Unknown mode', mode
 
-    detail = parse.parse(format_string_s, string)
+    detail = parse.parse(format_string_s, input_string)
     if detail is not None:
         return detail.named
     else:
         return None
 
 
-def processing_log(mode, string):
+def processing_log(mode, input_string):
 
-    error_info = log_parsers(mode, string)
+    error_info = log_parsers(mode, input_string)
 
     if error_info is None and mode is 1:
-        error_info = log_parsers(5, string)
+        error_info = log_parsers(5, input_string)
         # print detail_dict # test
 
     if error_info is not None:
         return error_info
     else:
         return None
-
