4c38e200c9f66c3c9c156fd06c14aba55db48752
luongli <luongintel2009@gmail.com>
2015-07-13 09:57:43 +0700

    Mod: reformat FORMAT_STRING

--- parsers.py
+++ parsers.py
@@ -2,67 +2,84 @@ import parse
 import sys
 
 
-FORMAT_STRING = {'haproxy': "{ip}:{port} \
-[{times}] \
-{frontend_name_transport} \
-{backend_name}/{server_name} \
-{tq}/{tw}/{tc}/{tr}/{tt} \
-{status_code} \
-{bytes_sent} \
-{captured_request_cookie} \
-{captured_response_cookie} \
-{country} \
-{actconn}/{feconn}/{beconn}/{srv_conn}/{retries} \
-{srv_queue}/{backend_queue} \
-{{{other}}} \"{http_request}\"", 'lighttpd': "{ip} \
-[{time}] \
-\"{http_request}\" \
-{status_code} \
-{byte_sent} \
-\"{referer}\" \
-\"{user_agent}\" \
-{byte_in} \
-{byte_out} \
-{time_used} \
-{request_hostname}", 'nginx': "{ip} \
-{http_x_forwarded_for} \
-[{time}] \
-{http_host} \
-\"{http_request}\" \
-{status_code} \
-{bytes_sent} \
-\"{http_referer}\" \
-{http_user_agentt}", 'ATS': "{time} RESPONSE: sent \
-{ip} status \
-{status_code} \
-({accelerator}) for \
-\'{http_referer}\'", 'haproxy2': "{ip}:{port} \
-[{times}] \
-{frontend_name_transport} \
-{backend_name}/{server_name} \
-{tq}/{tw}/{tc}/{tr}/{tt} \
-{status_code} \
-{bytes_sent} \
-{captured_request_cookie} \
-{captured_response_cookie} \
-{country} \
-{actconn}/{feconn}/{beconn}/{srv_conn}/{retries} \
-{srv_queue}/{backend_queue} \
-\"{http_request}\""}
+FORMAT_STRING = {
+    'HAPROXY':
+        "{ip}:{port} \
+        [{times}] \
+        {frontend_name_transport} \
+        {backend_name}/{server_name} \
+        {tq}/{tw}/{tc}/{tr}/{tt} \
+        {status_code} \
+        {bytes_sent} \
+        {captured_request_cookie} \
+        {captured_response_cookie} \
+        {country} \
+        {actconn}/{feconn}/{beconn}/{srv_conn}/{retries} \
+        {srv_queue}/{backend_queue} \
+        {{{other}}} \"{http_request}\"",
+
+    'LIGHTTPD':
+        "{ip} \
+        [{time}] \
+        \"{http_request}\" \
+        {status_code} \
+        {byte_sent} \
+        \"{referer}\" \
+        \"{user_agent}\" \
+        {byte_in} \
+        {byte_out} \
+        {time_used} \
+        {request_hostname}",
+
+    'NGINX':
+        "{ip} \
+        {http_x_forwarded_for} \
+        [{time}] \
+        {http_host} \
+        \"{http_request}\" \
+        {status_code} \
+        {bytes_sent} \
+        \"{http_referer}\" \
+        {http_user_agentt}",
+
+    'ATS':
+        "{time} RESPONSE: sent \
+        {ip} status \
+        {status_code} \
+        ({accelerator}) for \
+        \'{http_referer}\'",
+
+    'HAPROXY2':
+        "{ip}:{port} \
+        [{times}] \
+        {frontend_name_transport} \
+        {backend_name}/{server_name} \
+        {tq}/{tw}/{tc}/{tr}/{tt} \
+        {status_code} \
+        {bytes_sent} \
+        {captured_request_cookie} \
+        {captured_response_cookie} \
+        {country} \
+        {actconn}/{feconn}/{beconn}/{srv_conn}/{retries} \
+        {srv_queue}/{backend_queue} \
+        \"{http_request}\""
+}
 
 
-def parser_function(mode, string):
+def log_parsers(mode, string):
 
     if mode == 1:
-        format_string_s = FORMAT_STRING['haproxy']
-    if mode == 2:
-        format_string_s = FORMAT_STRING['lighttpd']
-    if mode == 3:
-        format_string_s = FORMAT_STRING['nginx']
-    if mode == 4:
+        format_string_s = FORMAT_STRING['HAPROXY']
+    elif mode == 2:
+        format_string_s = FORMAT_STRING['LIGHTTPD']
+    elif mode == 3:
+        format_string_s = FORMAT_STRING['NGINX']
+    elif mode == 4:
         format_string_s = FORMAT_STRING['ATS']
-    if mode == 5:
-        format_string_s = FORMAT_STRING['haproxy2']
+    elif mode == 5:
+        format_string_s = FORMAT_STRING['HAPROXY2']
+    else:
+        raise 'Unknown mode', mode
 
     detail = parse.parse(format_string_s, string)
     if detail is not None:
@@ -73,14 +90,14 @@ def parser_function(mode, string):
 
 def processing_log(mode, string):
 
-    detail_dict = parser_function(mode, string)
+    error_info = log_parsers(mode, string)
 
-    if detail_dict is None and mode is 1:
-        detail_dict = parser_function(5, string)
+    if error_info is None and mode is 1:
+        error_info = log_parsers(5, string)
         # print detail_dict # test
 
-    if detail_dict is not None:
-        return detail_dict
+    if error_info is not None:
+        return error_info
     else:
         return None
 
--- test_sentry.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'li'
-
-import sentry
-
-client = sentry.initialize_connection(
-    "http://ea8723b06f824d55b49031a702caa2c6:20fdb8b37c354c05b9c5c6ae1807c4a7@sentry.platform.vn/11"
-)
-
-sentry.push_to_sentry(client, "type1", {"IP": "1.1.1.1"})
